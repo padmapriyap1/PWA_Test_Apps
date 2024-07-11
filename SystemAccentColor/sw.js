fc7a475857dd57628c1a60c0e526f30f38b94296
padmapriyap1 <80009522+padmapriyap1@users.noreply.github.com>
2024-07-11 12:22:34 -0700

    Update sw.js

--- SystemAccentColor/sw.js
+++ SystemAccentColor/sw.js
@@ -9,35 +9,3 @@ self.addEventListener('fetch', e => {
     })
   );
 });
-
-function refreshColors() {
-  let dark = document.getElementById("dark");
-  let light = document.getElementById("light");
-  let controls = document.getElementById("control-color");
-  for (const box of document.getElementsByClassName("box")) {
-    box.querySelector("span").textContent = window.getComputedStyle(
-      box
-    ).backgroundColor;
-  }
-  controls.querySelector("span").textContent =      
-    window
-      .getComputedStyle(dark.checked ? dark : light)
-      .accentColor;
-}
-
-this.document.getElementById("dark").checked = Boolean(
-  window.matchMedia("(prefers-color-scheme:dark)").matches
-);
-
-function onSchemeChangeEvent({ target }) {
-  document.documentElement.setAttribute(
-    "color-scheme", 
-    target.value
-  );
-  refreshColors();
-}
-
-document.getElementById("light").onchange = onSchemeChangeEvent;
-document.getElementById("dark").onchange = onSchemeChangeEvent;
-
-refreshColors();
