93c666d28e48cac2853379ca2c6fa30ff7ae0a96
padmapriyap1 <80009522+padmapriyap1@users.noreply.github.com>
2024-07-11 11:38:43 -0700

    Update sw.js

--- SystemAccentColor/sw.js
+++ SystemAccentColor/sw.js
@@ -1,3 +1,17 @@
+this.addEventListener('install', async (event) => {
+  return;
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return new Response('Hello offline page');
+    })
+  );
+});
+
+
+
 let light = document.getElementById("light");
 let dark = document.getElementById("dark");
 const controlColor = document.getElementById("control-color");
